68e449728f5585d5fb639e263d24d8a6780805ef
AugustoCollado <AugustoCollado@455253e3-0956-0410-a266-0b799cdc475d>
2008-11-09 08:51:43 +0000

    abms funcionando: servicios y especialidades

--- trunk/src/java/digiturnos/dao/jdbc/EspecialidadesDaoImpl.java
+++ trunk/src/java/digiturnos/dao/jdbc/EspecialidadesDaoImpl.java
@@ -1,11 +1,6 @@
 package digiturnos.dao.jdbc;
 
 import digiturnos.dao.dao.*;
-import digiturnos.dao.dto.*;
-import digiturnos.dao.exception.*;
-import java.util.*;
-import java.sql.*;
-import org.apache.commons.logging.*;
 
 public class EspecialidadesDaoImpl extends EspecialidadesDaoImplBase implements EspecialidadesDao {
 
