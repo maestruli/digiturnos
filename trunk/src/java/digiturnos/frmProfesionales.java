f226b4791d22e97d2425ca40b5e8204c5fcf71d5
AugustoCollado <AugustoCollado@455253e3-0956-0410-a266-0b799cdc475d>
2008-11-10 19:55:33 +0000

    abms funcionando: servicios y especialidades

--- trunk/src/java/digiturnos/frmProfesionales.java
+++ trunk/src/java/digiturnos/frmProfesionales.java
@@ -170,7 +170,7 @@ public class frmProfesionales extends AbstractPageBean {
     public String cmdNuevo_action() {
         // TODO: Process the action. Return value is a navigation
         // case name where null will return to the same page.
-        return "nuevo";
+        return "nuevoProfesional";
     }
 
     public String lnkCerrarSesion_action() {
